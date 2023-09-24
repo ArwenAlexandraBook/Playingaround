23e5529ae2afb72d1e6d6e0f9b74af1a96751ac9
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2023-09-24 12:39:35 +0200

    Update main.c
    added some bits and pieces not yet done.

--- main.c
+++ main.c
@@ -14,30 +14,38 @@ const int numButton[] sizedof (buttonPin) / sizeof (buttonPin[0]);
 char currentText[5] = {0};
 char questionText [20] = " "; //Frågan som står på LCD-skärmen
 
+struct ButtonEvent {
+    int button;
+    unsigned long timestamp;
+};
+
+ButtonMemory buttonEvent [60] //Sparar knapptryckningar
 
 void HandleButtonClick(char *txt) {
-    _delay_ms(200);
-    strcat(currenText,txt);
-    lcd_set_cursor (0,1)
-    lcd_puts(currentText);
+    delay(200);
+    strcat(currentText, *txt) {
+        lcd_set_cursor(0, 1);
+        lcd_puts(currentText);
     return;
 
-
-if (strl(currentText)==4) {
-if (strcmp("1442",currentText)) 
-    lcd_printf("That's correct!"); 
+if (strlen(currentText) == 4) {
+    if (strcmp("1442",currentText)==0) {
+         lcd_printf("That's correct!"); 
 }else{
     lcd_printf("Incorrct code!"); //Felmeddelande
-    _delay_ms(3000);
-    lcd_set_cursor(0,1); //LCD-cursor rad 1
+    delay(3000);
+    lcd_set_cursor(0, 1); //LCD-cursor rad 1
     lcd_puts( "  ");
     lcd_set_cursor (0,1); 
 }
 currentText[0] = 0;
+}
+
+}
 
 void setup () {
     for (int i = 0; i <numButton; i++) {
-        pinMode(buttonPin[i], INPUT_PULLUP)
+        pinMode(buttonPin[i], INPUT_PULLUP);
     }
 
 millis_init();
@@ -46,18 +54,18 @@ lcd_init(); // Initialiserar LCD
 lcd_enable_blinking(); // Aktiverar blink
 lcd_enable_cursor(); // Aktiverar cursor
 
-lcd_set_cursor (0,0) //LCD-cursor rad 0 
+lcd_set_cursor (0, 0) //LCD-cursor rad 0 
 lcd_puts(questionText); //Frågan visas på lcd skärmen
 }
 
 
-while(1)
-{
-if RED_BUTTON_PIN {HandleButtonClick ("1"); // Röd knapp ger värde 1
-}
-if YELLOW_BUTTON_PIN {HandleButtonClick ("2"); //Guld knapp ger värde 2
-}
-if GREEN_BUTTON_PIN {HandleButtonClick ("3"); //Grön kanpp värde 3
-}
-return;     
+void loop() {
+
+for (int i =0; i <numButton; i++) {
+    if (digitalRead(buttonPin[i]) == LOW) {
+
+        if (numEvent < sideof(buttonEvent) / sizeof(buttonEvent[0]))
+        buttonEvent[numEvent].button =i;
+        buttonEvent[numEvents].timestamp =millis();
+        numEvents++;
 }
