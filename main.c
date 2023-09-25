7275bc5b0fa6c9a6f983b151464e72713db527fe
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2023-09-25 13:05:01 +0200

    Update main.c
    update 3

--- main.c
+++ main.c
@@ -1,71 +1,81 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 #include "lcd.h"
 
+#define RED_BUTTON_PIN 0    // Röda knappens pin
+#define YELLOW_BUTTON_PIN 1 // Gula knappens pin
+#define GREEN_BUTTON_PIN 2  // Gröna knappens pin
 
-#define RED_BUTTON_PIN 0  //Röda knappens pin
-#define YELLOW_BUTTON_PIN 1 //Gula knappens pin
-#define GREEN_BUTTON_PIN 2 // Gröna knappens pin
-
-const int buttonPin[] {RED_BUTTON_PIN, YELLOW_BUTTON_PIN, GREEN_BUTTON_PIN};
-const int numButton[] sizedof (buttonPin) / sizeof (buttonPin[0]);
+const int buttonPin[]{RED_BUTTON_PIN, YELLOW_BUTTON_PIN, GREEN_BUTTON_PIN};
+const int numButton[] sizeof(buttonPin) / sizeof(buttonPin[0]);
 
 char currentText[5] = {0};
-char questionText [20] = " "; //Frågan som står på LCD-skärmen
+char questionText[20] = " "; // Frågan som står på LCD-skärmen
 
-struct ButtonEvent {
+struct ButtonEvents
+{
     int button;
     unsigned long timestamp;
 };
 
-ButtonMemory buttonEvent [60] //Sparar knapptryckningar
+ButtonEvent buttonEvents[60] // Sparar knapptryckningar
+    int numEvents = 0;
 
-void HandleButtonClick(char *txt) {
+void HandleButtonClick(char *txt)
+{
     delay(200);
-    strcat(currentText, *txt) {
+    strcat(currentText, txt)
+    {
         lcd_set_cursor(0, 1);
         lcd_puts(currentText);
-    return;
 
-if (strlen(currentText) == 4) {
-    if (strcmp("1442",currentText)==0) {
-         lcd_printf("That's correct!"); 
-}else{
-    lcd_printf("Incorrct code!"); //Felmeddelande
-    delay(3000);
-    lcd_set_cursor(0, 1); //LCD-cursor rad 1
-    lcd_puts( "  ");
-    lcd_set_cursor (0,1); 
-}
-currentText[0] = 0;
-}
-
-}
-
-void setup () {
-    for (int i = 0; i <numButton; i++) {
-        pinMode(buttonPin[i], INPUT_PULLUP);
+        if (strlen(currentText) == 4)
+        {
+            if (strcmp("1442", currentText) == 0)
+            {
+                lcd_printf("That's correct!");
+            }
+            else
+            {
+                lcd_printf("Incorrct code!"); // Felmeddelande
+                delay(3000);
+                lcd_set_cursor(0, 1); // LCD-cursor rad 1
+                lcd_puts("  ");
+                lcd_set_cursor(0, 1);
+            }
+            currentText[0] = 0;
+        }
     }
 
-millis_init();
+    void setup()
+    {
+        for (int i = 0; i < numButton; i++)
+        {
+            pinMode(buttonPin[i], INPUT_PULLUP);
+        }
 
-lcd_init(); // Initialiserar LCD
-lcd_enable_blinking(); // Aktiverar blink
-lcd_enable_cursor(); // Aktiverar cursor
+        lcd_init();            // Initialiserar LCD
+        lcd_enable_blinking(); // Aktiverar blink
+        lcd_enable_cursor();   // Aktiverar cursor
 
-lcd_set_cursor (0, 0) //LCD-cursor rad 0 
-lcd_puts(questionText); //Frågan visas på lcd skärmen
-}
+        lcd_set_cursor(0, 0)        // LCD-cursor rad 0
+            lcd_puts(questionText); // Frågan visas på lcd skärmen
+    }
 
+    void loop()
+    {
 
-void loop() {
+        for (int i = 0; i < numButton; i++)
+        {
+            if (digitalRead(buttonPin[i]) == LOW)
+            {
 
-for (int i =0; i <numButton; i++) {
-    if (digitalRead(buttonPin[i]) == LOW) {
-
-        if (numEvent < sideof(buttonEvent) / sizeof(buttonEvent[0]))
-        buttonEvent[numEvent].button =i;
-        buttonEvent[numEvents].timestamp =millis();
-        numEvents++;
+                if (numEvent < sideof(buttonEvent) / sizeof(buttonEvents[0]))
+                    buttonEvents[numEvents].button = i;
+                buttonEvents[numEvents].timestamp = millis();
+                numEvents++;
+            }
+        }
+    }
 }
