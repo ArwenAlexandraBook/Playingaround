9022d2da387879a0fbec0c9dae297429f79a8372
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2023-10-17 10:55:22 +0200

    Add files via upload

---
+++ uart.c
@@ -0,0 +1,47 @@
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/setbaud.h>
+
+#define BAUD 9600 // Baudrate hastigheten
+
+
+// Funktion som initierar uart
+void uart_init(void)
+{
+    UBRR0H = UBBRH_VALUE;
+    UBRR0L = UBBRRL_VALUE;
+
+#if USE_2X
+    UCSR0A = |= _BV(U2x0);
+#else
+    UCSR0A &= ~(_BV(USX0));
+#endif
+
+ UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bitar data */ 
+ UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Aktivera RX and TX */  
+}
+int uart_putchar (char c, FILE *stream)
+{
+  loop_until_bit_is_set(USCR0A, UDRE0);
+  UBR0 = c;
+  return(0);
+}
+
+// Läser character från UART 
+int uart_getchar (FILE *stream)
+{
+    loop_until_bit_is_set(USCR0A, RXC0);
+    return UDR0;
+}
+
+FILE uart_output = FDEV_SETUP_STREAM(uart_putchar,NULL, _FDEV_SETUP_WRITE)
+FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+
+
+// Funktion för seriell kommunikation
+void init_serial()
+{
+    uart_init();
+    stdout = &uart_output;
+    stdin = &uart_input;
+}
